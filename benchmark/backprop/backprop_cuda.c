474a4444b438a4afa23a276b170021f19a4b026d
zouan616 <zouan616@gmail.com>
2019-09-10 15:50:26 -0500

    benchmakr

---
+++ benchmark/backprop/backprop_cuda.c
@@ -0,0 +1,127 @@
+
+
+// includes, system
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+//#include <cuda.h>
+#include <sys/time.h>
+
+// includes, kernels
+//#include <backprop_cuda_kernel.cu>
+#include "backprop.h"
+
+////////////////////////////////////////////////////////////////////////////////
+
+extern "C"
+void bpnn_layerforward(float *l1, float *l2, float **conn, int n1, int n2);
+
+
+extern "C"
+void bpnn_output_error(float *delta, float *target, float *output, int nj, float *err);
+
+extern "C"
+void bpnn_hidden_error(float *delta_h, int nh, float *delta_o, int no, float **who, float *hidden, float *err);
+
+extern "C" 
+void bpnn_adjust_weights(float *delta, int ndelta, float *ly, int nly, float **w, float **oldw);
+
+
+extern "C"
+int setup(int argc, char** argv);
+
+extern "C"
+float **alloc_2d_dbl(int m, int n);
+
+extern "C"
+float squash(float x);
+
+double gettime() {
+  struct timeval t;
+  gettimeofday(&t,NULL);
+  return t.tv_sec+t.tv_usec*1e-6;
+}
+
+unsigned int num_threads = 0;
+unsigned int num_blocks = 0;
+
+////////////////////////////////////////////////////////////////////////////////
+// Program main
+////////////////////////////////////////////////////////////////////////////////
+int
+main( int argc, char** argv) 
+{
+	setup(argc, argv);
+}
+
+
+extern "C"
+void bpnn_train_cuda(BPNN *net, float *eo, float *eh)
+{
+  int in, hid, out;
+  float out_err, hid_err;
+  
+  in = net->input_n;
+  hid = net->hidden_n;
+  out = net->output_n;   
+   
+#ifdef GPU  
+  int m = 0;
+  float *input_hidden_cuda;
+  float *input_cuda;
+  float *output_hidden_cuda;
+  float *partial_sum;
+  float *hidden_partial_sum;
+  float *hidden_delta_cuda;
+  float *input_prev_weights_cuda;
+  float sum;
+  float *input_weights_one_dim;
+  float *input_weights_prev_one_dim;
+  num_blocks = in / 16;  
+
+  
+  input_weights_one_dim = (float *) malloc((in + 1)* (hid + 1) * sizeof(float));
+  input_weights_prev_one_dim = (float *) malloc((in + 1)* (hid + 1) * sizeof(float));
+  partial_sum = (float *) malloc(num_blocks * WIDTH * sizeof(float));
+ 
+  // this preprocessing stage is added to correct the bugs of wrong memcopy using two-dimensional net->inputweights
+  for (int k = 0; k <= in; k++) {	
+   for (int j = 0; j <= hid; j++) {
+	  input_weights_one_dim[m] = net->input_weights[k][j];
+	  input_weights_prev_one_dim[m] = net-> input_prev_weights[k][j];
+	  m++;
+    }
+  }
+  
+#endif
+
+#ifdef CPU
+
+  printf("Performing CPU computation\n");
+  bpnn_layerforward(net->input_units, net->hidden_units,net->input_weights, in, hid);
+
+#endif
+
+
+  bpnn_layerforward(net->hidden_units, net->output_units, net->hidden_weights, hid, out);
+  bpnn_output_error(net->output_delta, net->target, net->output_units, out, &out_err);
+  bpnn_hidden_error(net->hidden_delta, hid, net->output_delta, out, net->hidden_weights, net->hidden_units, &hid_err);  
+  bpnn_adjust_weights(net->output_delta, out, net->hidden_units, hid, net->hidden_weights, net->hidden_prev_weights);
+
+#ifdef CPU
+
+  bpnn_adjust_weights(net->hidden_delta, hid, net->input_units, in, net->input_weights, net->input_prev_weights);
+
+#endif  
+
+
+  free(partial_sum);
+  free(input_weights_one_dim);
+  free(input_weights_prev_one_dim);
+
+  
+  
+  
+
+}
